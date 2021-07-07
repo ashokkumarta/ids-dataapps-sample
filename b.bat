cc0e16a3e9a8bff8c3b030eefc414408da0d7f76
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-07 23:01:23 +0530

    Batch script change

--- b.bat
+++ b.bat
@@ -4,37 +4,37 @@ call docker image rm ashokkumarta/dataapps-sample:latest
 call docker build -t ashokkumarta/dataapps-sample:latest .
 call docker push ashokkumarta/dataapps-sample:latest
 
-call cd ..\ids-test-dataapps\ids-test-provider
+call cd ..\ids-test-dataapps\test-provider
 call docker image rm ashokkumarta/test_provider:latest
 call docker build -t ashokkumarta/test_provider:latest .
 call docker push ashokkumarta/test_provider:latest
 
-call cd ..\ids-test-consumer
+call cd ..\test-consumer
 call docker image rm ashokkumarta/test_consumer:latest
 call docker build -t ashokkumarta/test_consumer:latest .
 call docker push ashokkumarta/test_consumer:latest
 
-call cd ..\ids-test-processor
+call cd ..\test-processor
 call docker image rm ashokkumarta/test_processor:latest
 call docker build -t ashokkumarta/test_processor:latest .
 call docker push ashokkumarta/test_processor:latest
 
-call cd ..\ids-test-generic
+call cd ..\test-generic
 call docker image rm ashokkumarta/test_generic:latest
 call docker build -t ashokkumarta/test_generic:latest .
 call docker push ashokkumarta/test_generic:latest
 
-call cd ..\ids-test-auditor
+call cd ..\test-auditor
 call docker image rm ashokkumarta/test_auditor:latest
 call docker build -t ashokkumarta/test_auditor:latest .
 call docker push ashokkumarta/test_auditor:latest
 
-call cd ..\ids-test-encryptor
+call cd ..\test-encryptor
 call docker image rm ashokkumarta/test_encryptor:ltest
 call docker build -t ashokkumarta/test_encryptor:latest .
 call docker push ashokkumarta/test_encryptor:latest
 
-call cd ..\ids-test-validator
+call cd ..\test-validator
 call docker image rm ashokkumarta/test_validator:latest
 call docker build -t ashokkumarta/test_validator:latest .
 call docker push ashokkumarta/test_validator:latest
